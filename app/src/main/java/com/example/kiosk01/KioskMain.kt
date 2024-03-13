07b251c9e0109ff3ebb591e6a6626b42559c4085
hyejeong <112006114+hyezg@users.noreply.github.com>
2024-03-13 21:02:29 +0900

    first

---
+++ app/src/main/java/com/example/kiosk01/KioskMain.kt
@@ -0,0 +1,168 @@
+package com.example.kiosk01
+fun main() {
+        val or = Order()
+        or.menu()
+
+        while(true) {
+            val num = readLine()!!.toInt()
+
+            if (num !in 0..3) {
+                println("0~3까지 입력가능합니다.")
+                main()
+            }
+            val hamN = Ham()
+            val pizN = Piz()
+
+            println("====")
+            /*if(num == 1){
+                hamN.select()
+            }*/
+            when (num) {
+                1 -> hamN.select()
+
+                2 -> pizN.select()
+
+                3 -> hamN.select()
+                //잠시만 ham으로
+
+                0 -> {
+                    println("종료 합니다.")
+                    break
+                }
+            }
+        }
+
+}
+class Order {
+    fun menu() {
+        println("===메뉴판===")
+        println("1. 햄버거")
+        println("2. 피자")
+        println("3. 치킨")
+        println("0. 종료")
+        print("번호를 입력하세요: ")
+    }
+}
+
+open class Food {
+    var name =""
+    var price = 0
+    var material = ""
+
+    fun ordermenu() {
+        val num2 = readln().toInt()
+        when (num2) {
+            1,2,3 -> main()
+            0 -> {
+                println("뒤로가기입니다.")
+                main()
+            }
+        }
+    }
+
+    fun hambuger(): String {
+        name = "햄버거"
+        price = 100000
+        material = "기본 재료"
+        return "$name, $price, $material"
+    }
+}
+open class Ham : Food() {
+    init {
+        println("===햄버거===")
+        println("1.${BurgerA()}")
+        println("2.${BurgerB()}")
+        println("3.${BurgerC()}")
+        println("가장 기본은 ${hambuger()}")
+        println("0. 뒤로가기")
+    }
+    fun select(){
+        println("셀렉트입니다. 번호를 고르세요")
+        ordermenu()
+
+    }
+
+
+    fun BurgerA(): String {
+        name = "burger A"
+        price = 9000
+        material = "토마토, 체리 페퍼에 쉑소스가.."
+        return "$name | $price 원 | $material"
+    }
+    fun BurgerB(): String {
+        name = "burger B"
+        price = 10000
+        material = "토마토,   체리 페퍼에 쉑소스가.."
+        return "$name| $price 원 | $material"
+    }
+    fun BurgerC(): String {
+        name = "burger C"
+        price = 8000
+        material = "토마토,   체리 페퍼에 쉑소스가.."
+        return "$name| $price 원 | $material"
+    }
+    /*open fun burgerlist(name:String, price:Int, material:String) {
+            println("${name}, ${price}, ${material}")
+    }*/
+}
+
+
+open class Piz : Food() {
+    /*var name =""
+    var price = 0
+    var material = ""*/
+
+    init {
+        println("===피자===")
+        println("1.${PizzaA()}")
+        println("2.${PizzaB()}")
+        println("3.${PizzaC()}")
+        println("0. 뒤로가기")
+    }
+    fun select(){
+        println("셀렉트입니다. 번호를 고르세요")
+        ordermenu()
+    }
+
+    fun PizzaA(): String {
+        name = "pizz A"
+        price = 9000
+        material = "토마토, 체리 페퍼에 쉑소스가.."
+        return "$name | $price 원 | $material"
+    }
+    fun PizzaB(): String {
+        name = "pizza B"
+        price = 10000
+        material = "토마토,   체리 페퍼에 쉑소스가.."
+        return "$name| $price 원 | $material"
+    }
+    fun PizzaC(): String {
+        name = "pizza C"
+        price = 8000
+        material = "토마토,   체리 페퍼에 쉑소스가.."
+        return "$name| $price 원 | $material"
+    }
+
+}
+
+/*class BurgerA : Ham() {
+    override fun burgerlist(name: String, price: Int, material: String) {
+        super.burgerlist("buger A", 9000, "토마토, 체리 페퍼에 쉑소스가..")
+
+    }
+}
+
+class BurgerB : Ham() {
+    override fun burgerlist(name: String, price: Int, material: String) {
+        super.burgerlist("buger b", 9000, "토마토, 체리 페퍼에 쉑소스가..")
+
+    }
+}
+
+class BurgerC: Ham() {
+    override fun burgerlist(name: String, price: Int, material: String) {
+        super.burgerlist("buger C", 9000, "토마토, 체리 페퍼에 쉑소스가..")
+
+    }
+}*/
+
